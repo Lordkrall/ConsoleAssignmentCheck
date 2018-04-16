968896f920fe913fab34b9b74f80735ebf5c0769
Jonathan Krall Olsson <jonathan_olsson88@hotmail.com>
2018-04-16 09:22:38 +0200

    Initial upload

---
+++ ConsoleAssignmentCheck/Exercises.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAssignmentCheck
+{
+    public class Exercises
+    {
+
+        public void Exercise1()
+        {
+            string firstName = "Jonathan";
+            string lastName = "Krall";
+            Console.WriteLine("Hello {0} {1}! I'm glad to inform you that you are the test subject of my very first assignment!", firstName, lastName);
+            Console.ReadKey();
+        }
+        public void Exercise2()
+        {
+            DateTime yesterday = DateTime.Now.Date.AddDays(-1);
+            DateTime today = DateTime.Now.Date;
+            DateTime tomorrow = DateTime.Now.Date.AddDays(1);
+
+            Console.WriteLine("Todays date is: {0} \nTomorrows date is: {1} \nYesterdays date was {2}", today.ToShortDateString(), tomorrow.ToShortDateString(), yesterday.ToShortDateString());
+            Console.ReadKey();
+
+        }
+        public void Exercise3()
+        {
+            Console.Write("Please enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Please enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine(firstName + " " + lastName);
+            Console.ReadKey();
+        }
+        public void Exercise4()
+        {
+            string originalText = "The quick fox Jumped Over the DOG";
+            string changedText = "";
+            changedText = originalText.ToLower();
+            changedText = changedText.Remove(0, 1);
+            changedText = changedText.Insert(0, "T");
+            changedText = changedText.Replace("quick", "brown");
+            int indexOfDog = changedText.IndexOf("dog");
+            changedText = changedText.Insert(indexOfDog, "lazy ");
+
+            Console.WriteLine(originalText);
+            Console.WriteLine(changedText);
+            Console.ReadKey();
+
+        }
+        public void Exercise5()
+        {
+            string str = "Arrays are very common in programming, they look something like: [1,2,3,4,5]";
+            str = str.Substring(str.IndexOf("["), 11);
+            str = str.Insert(str.Length - 1, ",6,7,8,9,10");
+            str = str.Replace("2,3,", "");
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+        public void Exercise6()
+        {
+            Console.Write("Please write a number: ");
+            bool ba = int.TryParse(Console.ReadLine(), out int a);
+            Console.Write("Please write another number: ");
+            bool bb = int.TryParse(Console.ReadLine(), out int b);
+            int biggest;
+            int smallest;
+
+            if (a > b)
+            {
+                biggest = a;
+                smallest = b;
+            }
+            else
+            {
+                biggest = b;
+                smallest = a;
+            }
+            double sum = a + b;
+            double product = a * b;
+            double difference = a - b;
+            double ratio = a / b;
+
+
+            Console.WriteLine("The smallest: {0}\nThe biggest: {1}\nThe sum: {2}\nThe difference: {3}\nThe product: {4}\nThe ratio: {5}", smallest, biggest, sum, difference, product, ratio);
+            Console.ReadKey();
+
+        }
+        public void Exercise7()
+        {
+            Console.Write("Please input the radius: ");
+            bool possible = double.TryParse(Console.ReadLine(), out double input);
+
+            double circleArea = Math.PI * Math.Pow(input, 2);
+            double sphereVolume = (4.0 / 3) * Math.PI * Math.Pow(input, 3);
+            Console.WriteLine("Area of the circle is: {0}\nVolume of the sphere is: {1}", circleArea, sphereVolume);
+            Console.ReadKey(); 
+        }
+        public void Exercise8()
+        {
+            Console.Write("Input a decimal value: ");
+            double.TryParse(Console.ReadLine(), out double input);
+            double sqrt = Math.Sqrt(input);
+            double pow2 = Math.Pow(input, 2);
+            double pow10 = Math.Pow(input, 10);
+
+            Console.WriteLine("The square root of {0} is: {1}\nThe power of 2 is:{2}\nThe power of 10 is:{3}", input, sqrt, pow2, pow10);
+            Console.ReadKey();
+        }
+        public void Excercise9()
+        {
+            Console.WriteLine("There is no exercise 9");
+            Console.ReadKey();
+        }
+        public void Exercise10()
+        {
+           bool isAlive = true;
+            while (isAlive)
+            {
+                Console.Clear(); 
+                Console.WriteLine("Hello user, please select one of these three options:\n1:Division \n2:Exercise 4 \n3:Change foreground color\n-1 to exit");
+                bool valid = int.TryParse(Console.ReadLine(), out int input);
+                if (!valid)
+                    Console.WriteLine("Please select a number!");
+
+                switch (input)
+                {
+                    case 1:
+                        Console.Write("Please give me a number: ");
+                        int.TryParse(Console.ReadLine(), out int a);
+                        Console.Write("Please give me another number: ");
+                        int.TryParse(Console.ReadLine(), out int b);
+                        Console.Write(Division(a, b));
+                        Console.ReadKey(); 
+                        break;
+                    case 2:
+                        Exercise4();
+                        break;
+                    case 3:
+                        ChangeForegroundColor(); 
+                        break;
+                    case -1:
+                        isAlive = false;
+                        break;                       
+                    default:
+                        break;
+                }
+            }
+
+
+        }
+        public void Exercise11()
+        {
+            Console.Write("Please enter a number: ");
+            
+            if (int.TryParse(Console.ReadLine(), out int input))
+            {
+                IterationMethod(input);
+            }
+            else
+            Console.WriteLine("Please enter a numeric value");
+
+            Console.ReadKey();
+            Console.ResetColor();
+        }  
+        public void Exercise12()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int x = 1; x <= 10; x++)
+                {
+                    Console.Write(i * x + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(); 
+        }
+        public void Exercise13()
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            int randomNumber = rnd.Next(1, 500);
+            bool correctGuess = false;
+            int numberOfGuesses = 0;
+            while (!correctGuess)
+            {
+                
+                Console.Write("Please guess a number between 1 and 500: ");
+                int.TryParse(Console.ReadLine(), out int guess);
+
+                if (guess > randomNumber)
+                {
+                    Console.WriteLine("Your guess of {0} was too high, please try again!",guess);
+                    numberOfGuesses++;
+                }
+                else if(guess < randomNumber)
+                {
+                    Console.WriteLine("Your guess of {0} was too low, please try again!",guess);
+                    numberOfGuesses++;
+                }
+                else
+                {
+                    numberOfGuesses++;
+                    Console.WriteLine("Your guess of {0} is correct!! It took you {1} tries!",guess,numberOfGuesses);
+                    Console.ReadKey();
+                    correctGuess = true;
+                }
+            }
+        }
+
+
+        private void IterationMethod(int input)
+        {
+            if (input > 0)
+            {
+                for (int i = 0; i < input + 1; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green; 
+                    }
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("----------------");
+                for (int i = input; i > 0; i--)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.WriteLine(i);
+                }
+            }
+            else
+                Console.WriteLine("Please enter a number that is greater than 0");
+        }
+
+        private void ChangeForegroundColor()
+        {
+            if (Console.ForegroundColor == ConsoleColor.Gray)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+
+        private string Division(int a, int b)
+        {
+            string resultString;
+            if (b == 0)
+            {
+                resultString = "You cannot divide by zero!";
+            }
+            else
+            {
+                int result = a / b;
+                resultString = string.Format("{0}/{1} = {2}", a, b, result);
+            }
+            return resultString;
+        }
+    }
+}
---
+++ ConsoleAssignmentCheck/Program.cs
@@ -0,0 +1,105 @@
+﻿using ConsoleAssignmentCheck;
+using System;
+
+namespace Execise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercises ex = new Exercises();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Exercise 1 \n2. Exercise 2 \n3. Exercise 3 \n4. Exercise 4 \n" +
+               "5. Exercise 5 \n6. Exercise 6 \n7. Exercise 7 \n8. Exercise 8 \n9. Exercise 9 \n10. Exercise 10 \n11. Exercise 11 \n12. Exercise 12 \n" +
+               "13. Exercise 13 \nWhich exercise do you want to check? (-1 to exit)");
+
+                bool validNumber = int.TryParse(Console.ReadLine(), out int selection);
+                if (!validNumber)
+                    selection = 0;
+
+                switch (selection)
+                {
+                    case 1:
+                        Console.Clear();
+                        ex.Exercise1();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        ex.Exercise2();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        ex.Exercise3();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        ex.Exercise4();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        ex.Exercise5();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        ex.Exercise6();
+                        break;
+                    case 7:
+                        Console.Clear();
+                        ex.Exercise7();
+                        break;
+                    case 8:
+                        Console.Clear();
+                        ex.Exercise8();
+                        break;
+                    case 9:
+                        Console.Clear();
+                        ex.Excercise9();
+                        break;
+                    case 10:
+                        Console.Clear();
+                        ex.Exercise10();
+                        break;
+                    case 11:
+                        Console.Clear();
+                        ex.Exercise11();
+                        break;
+                    case 12:
+                        Console.Clear();
+                        ex.Exercise12();
+                        break;
+                    case 13:
+                        Console.Clear();
+                        ex.Exercise13();
+                        break;
+                    case 14:
+                        break;
+                    case 15:
+                        break;
+                    case 16:
+                        break;
+                    case 17:
+                        break;
+                    case 18:
+                        break;
+                    case 19:
+                        break;
+                    case 20:
+                        break;
+                    case -1:
+                        Console.Clear();
+                        Console.WriteLine("Thank you for using our program, have a nice day!");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("{0}, is not a valid selection, please try again", selection);
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
